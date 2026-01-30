5cd6f2de9fae18da88c354c5fabd2de449e84c39
Thiago Scheffer <thiago.sc.cardoso@gmail.com>
2026-01-31 02:03:42 +1300

    Start of the project (study)

---
+++ cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/ThiagoScheffer/azure-tagger-api/internal/handlers"
+	"github.com/ThiagoScheffer/azure-tagger-api/internal/store"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	st := store.NewInMemoryStore()
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(middleware.Timeout(60 * time.Second))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.RequestID)
+
+	router.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	handler := handlers.New(st)
+
+	router.Route("/v1", func(r chi.Router) {
+		router.Get("/resources", handler.GetResources)
+		router.Post("/resources", handler.CreateResource)
+		router.Get("/resources/{id}", handler.GetResource)
+		//r.Put("/resources/{id}/tags", handler.UpdateResourceTags)
+		router.Delete("/resources/{id}", handler.DeleteResource)
+
+		router.Post("/resources/{id}/apply-tags", handler.ApplyTagsToAzure)
+	})
+
+	log.Printf("Starting server on port %s", port)
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
+}
